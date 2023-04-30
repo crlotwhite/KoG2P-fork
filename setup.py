6ded0383e7bf93f1640aedca87aceb6ade2c7321
crlotwhite <crlotwhite@gmail.com>
2023-04-30 17:54:33 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='kog2p-fork',
+    version='0.0.1',
+    description='fork version of KoG2P',
+    url='https://github.com/crlotwhite/KoG2P-fork.git',
+    author='crlotwhite',
+    author_email='crlotwhite@gmail.com',
+    license='GPL-3.0 license',
+    packages=['kog2p'],
+    zip_safe=False,
+    install_requires=[]
+)
